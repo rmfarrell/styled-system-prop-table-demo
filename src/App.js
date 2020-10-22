0330447e0bbd73985b8c84133737c49d1b605a14
Ryan <rmfarrell@gmail.com>
2020-10-22 15:12:31 -0400

    add code block

--- src/App.js
+++ src/App.js
@@ -9,7 +9,26 @@ import Card from './Card'
 import { ThemeProvider } from 'styled-components'
 import theme from './lib/theme'
 import styled from 'styled-components'
-import { color, space } from 'styled-system'
+import { color, layout, space } from 'styled-system'
+import { StyledSystemPropsTable as PropsTable } from 'styled-system-props-table'
+import { CodeBlock, dracula } from "react-code-blocks";
+
+const transformProps = (name = '', properties = {}) => {
+  if (name === 'variant') return {
+    ...properties,
+    description: <>
+      <div><strong>Card variants</strong></div>
+      <ul>
+        <li><strong>product</strong> large card, lots of padding horizontally stacked</li>
+        <li><strong>product-small</strong> small card for sidebars</li>
+        <li><strong>info</strong> small card with sharp corners and no box-shadow</li>
+      </ul>
+    </>
+  }
+}
+
+const code = `
+import Card from './Card'
 import { StyledSystemPropsTable as PropsTable } from 'styled-system-props-table'
 
 const transformProps = (name = '', properties = {}) => {
@@ -26,10 +45,33 @@ const transformProps = (name = '', properties = {}) => {
   }
 }
 
+return () => {
+  <Card maxWidth={950} mx="auto" my={2} px={0} py={2} bg="white">
+    <PropsTable
+      component={Card}
+      theme={theme}
+      transformProps={transformProps}
+    />
+  </Card>
+}
+
+`
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Main bg="champagne" py={4}>
+        <CodeContainer
+          maxWidth={950}
+          mx="auto"
+          mb={4}>
+          <CodeBlock
+            text={code}
+            language="jsx"
+            theme={dracula}
+            showLineNumbers={false}
+          />
+        </CodeContainer>
         <Card maxWidth={950} mx="auto" my={2} px={0} py={2} bg="white">
           <PropsTable
             component={Card}
@@ -41,6 +83,11 @@ function App() {
     </ThemeProvider>
   );
 }
+
+const CodeContainer = styled.div`
+  ${space};
+  ${layout}
+`
 
 const Main = styled.main`
   min-height: 100vh;
